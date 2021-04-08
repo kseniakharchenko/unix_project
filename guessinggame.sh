b8c1cdc65512a34cb7f5f497c43fe363cae94a15
kseniakharchenko <75413213+kseniakharchenko@users.noreply.github.com>
2021-04-08 17:43:38 +0400

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -5,20 +5,5 @@ correct_number=$(ls | wc -l)
 echo "Hello, please guess the number of files in the current directory:"
 read response
 
-function guessing {
-	while [[ $response -ne $correct_number ]]
-	do
-		if [[ $response -lt $correct_number ]]
-		then
-			echo "$response is less than needed. Please enter a number again:"
-			read response
-		elif [[ $response -gt $correct_number ]]
-		then
-			echo "$response is more than needed. Please enter a number again:"
-			read response
-		fi
-	done
-	echo "You are right!"
-}
 
 guessing response correct_number
